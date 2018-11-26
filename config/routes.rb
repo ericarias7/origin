a89653d0280025819ed3356f6c8c406e705219dc
ericarias7 <ariaseric7@gmail.com>
2018-11-25 22:31:18 -0800

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
- resources :posts
+ devise_for :users, :controllers => { registrations: 'registrations' }
  root 'posts#index'
-
+ resources :posts do
+ 	resources :comments
+ end
 end
